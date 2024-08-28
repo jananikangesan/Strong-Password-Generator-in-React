83590ca3c606ebb0e37b74f5fa389edaded76823
jananikangesan <janakanges1996@gmail.com>
2024-08-28 16:59:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import PasswordGenerator from './components/PasswordGenerator'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <PasswordGenerator/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
